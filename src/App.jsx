3fccba43d2fa4515c855e5e5c0492d74dd4cc1bf
annesilv4 <carolaynebarbozasilva@gmail.com>
2025-12-30 18:27:16 -0300

    feat: add search page route and product context wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+// Importações do React Router para gerenciamento de rotas
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Importações das páginas do aplicativo
 import HomePage from './pages/home';
+import SearchPage from './pages/search';
+
+// Importação do contexto para gerenciar estado global de produtos
+import { ProductProvider } from './context/productContext';
+
+// Importação do arquivo global de estilos CSS
 import './global.css';
 
+// Componente raiz da aplicação
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-      </Routes>
-    </Router>
+    // ProductProvider envolve toda a aplicação para disponibilizar o contexto de produtos
+    <ProductProvider>
+      {/* Router configura o sistema de roteamento da aplicação */}
+      <Router>
+        {/* Routes define as rotas disponíveis */}
+        <Routes>
+          {/* Rota para a página inicial - caminho raiz '/' */}
+          <Route path='/' element={<HomePage />} />
+          
+          {/* Rota para a página de busca */}
+          <Route path='/search' element={<SearchPage />} />
+        </Routes>
+      </Router>
+    </ProductProvider>
   )
 }
